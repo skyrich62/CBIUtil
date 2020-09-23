a395c94f4906268ff13a8d27a6298181c3024576
Rich Newman <skyrich62@nraonline.com>
2020-09-22 17:40:39 -0700

    Add hash function for std::pair

---
+++ include/compubrite/hash_util.h
@@ -0,0 +1,23 @@
+#ifndef COMPUBRITE_HASH_UTIL_H_INCLUDED
+#define COMPUBRITE_HASH_UTIL_H_INCLUDED
+
+#include <functional>
+#include <utility>
+
+namespace std {
+    template <typename T>
+    size_t make_hash(const T &t)
+    {
+        return hash<T>()(t);
+    }
+
+    template <typename T, typename U>
+    struct hash< pair<T, U> > {
+        size_t operator()(const pair<T, U> &p) const
+        {
+            return make_hash(p.first) ^ (make_hash(p.second) << 1);
+        }
+    };
+}
+
+#endif // COMPUBRITE_HASH_UTIL_H_INCLUDED
