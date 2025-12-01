c0ae26ceacd46be0bc6979a864e9e8f9623d7c23
Rich Newman <skyrich62@users.noreply.github.com>
2025-12-01 13:42:09 -0800

    Add copyright and correct namespace for internal_stack.h

--- include/CompuBrite/internal_stack.h
+++ include/CompuBrite/internal_stack.h
@@ -1,8 +1,36 @@
+/**
+ * The MIT License (MIT)
+ *
+ * @copyright
+ * Copyright (c) 2020 Rich Newman
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * @file
+ * @brief Interface for internal_stack.
+*/
+
 #ifndef COMPUBRITE_INTERNAL_STACK_H_INCLUDED
 #define COMPUBRITE_INTERNAL_STACK_H_INCLUDED
 
 #include <memory>
 #include "CompuBrite/CheckPoint.h"
+
+namespace CompuBrite {
 
 /// Manage a stack of elements which is actually kept on the program stack.
 ///
@@ -110,5 +138,7 @@ private:
 
 template<typename T>
 internal_stack<T>* internal_stack<T>::_top = nullptr;
+
+} // namespace CompuBrite
 
 #endif // COMPUBRITE_INTERNAL_STACK_H_INCLUDED
